ba0d6688ca542ec6c47a5cc0398c82ef97a8c913
Kun Zhou <zhoukun@u.nus.edu>
2020-11-13 16:14:37 +0800

    Update convert.sh

--- interspeech2020_codes/convert.sh
+++ interspeech2020_codes/convert.sh
@@ -2,13 +2,13 @@
 
 cd ./vaw-gan-f0
 
-python convert-vawgan-separate-f0.py --checkpoint "./log/train/MODEL_NAME/model.ckpt-46860"
+python convert-vawgan-separate-f0.py #Please custominze your checkpoint dir: --checkpoint "./log/train/[TIMESETP]/model.ckpt-[MODEL ID]"
 python save_f0.py
 
 cd ../vaw-gan
 
 python test-condition.py
-python convert-vawgan-cwt.py --checkpoint "./log/train/MODEL_NAME/model.ckpt-46860"
+python convert-vawgan-cwt.py #Please custominze your checkpoint dir: --checkpoint "./log/train/[TIMESETP]/model.ckpt-[MODEL ID]"
 
 
 
